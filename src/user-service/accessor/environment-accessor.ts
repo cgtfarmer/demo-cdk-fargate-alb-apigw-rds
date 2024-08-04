cce9d02eae4288388d9c020550c628fe4c9c9501
Christian Farmer <christian.gt.farmer@gmail.com>
2024-08-04 14:02:06 -0500

    Updates

--- src/user-service/accessor/environment-accessor.ts
+++ src/user-service/accessor/environment-accessor.ts
@@ -4,7 +4,7 @@ export default class EnvironmentAccessor {
   public get(key: string) {
     const value: string | undefined = process.env[key];
 
-    if (!value) throw new Error(`${key} is undefined`);
+    if (!value) return '';
 
     return value;
   }
